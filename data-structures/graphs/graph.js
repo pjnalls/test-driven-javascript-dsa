20a6510c5c529de4f5415427be39d42f4330300a
pjnalls <pjnalls.js@gmail.com>
2021-06-10 05:39:25 -0500

    Implement `Graph` `data-structure`.

---
+++ data-structures/graphs/graph.js
@@ -0,0 +1,53 @@
+const testingUtils = require("../../utilities/testing-utils");
+const testCases = [
+  [
+    ["Fullstack", "Starbucks", 
+ "Insomnia Cookies", "Cafe Grumpy",
+ "Dublinger", "Dig Inn"], 
+    [
+      ["Fullstack", "Dublinger", 2],
+      ["Fullstack", "Dig Inn", 7],
+      ["Fullstack", "Starbucks", 6],
+      ["Dublinger", "Starbucks", 3],
+      ["Dublinger", "Dig Inn", 4],
+      ["Dublinger", "Insomnia Cookies", 7],
+      ["Starbucks", "Insomnia Cookies", 6],
+      ["Cafe Grumpy", "Insomnia Cookies", 5],
+      ["Cafe Grumpy", "Dig Inn", 9]
+    ],
+    `Graph.nodes = [${["Fullstack", "Starbucks", 
+    "Insomnia Cookies", "Cafe Grumpy",
+    "Dublinger", "Dig Inn"].toString()}]`
+  ]
+];
+
+function Graph () {
+  this.nodes = [];
+  this.adjacencyList = {}
+}
+
+Graph.prototype.addNode = function (node) {
+  this.nodes.push(node);
+  this.adjacencyList[node] = [];
+};
+
+Graph.prototype.addEdge = 
+  function (node1, node2, weight) {
+    this.adjacencyList[node1].push(
+      { node: node2, weight: weight }
+    );
+    this.adjacencyList[node2].push(
+      { node: node1, weight: weight }
+    );
+};
+
+const graphCreated = (testCase) => {
+  const graph = new Graph();
+  testCase[0].forEach((node) => graph.addNode(node));
+  testCase[1].forEach((edge) => graph.addEdge(...edge));
+  return `Graph.nodes = [${graph.nodes.toString()}]`;
+}
+
+testingUtils.runTestsTo(graphCreated, testCases);
+
+module.exports = exports = { Graph };
