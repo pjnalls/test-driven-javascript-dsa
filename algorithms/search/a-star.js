f291c0a99445bc968a16ec7a4437edd2f2d6bf0b
pjnalls <pjnalls.js@gmail.com>
2021-06-10 14:08:02 -0500

    Implement `A*` algo.; TODO: Debug `totalPath`.

---
+++ algorithms/search/a-star.js
@@ -0,0 +1,132 @@
+/**
+ *
+ * Credits to Adrienne Johnson for original
+ * `Graph`, `PriorityQueue`, and test case
+ * implementation which you can view here:
+ * https://medium.com/@adriennetjohnson/a-walkthrough-of-dijkstras-algorithm-in-javascript-e94b74192026
+ *
+ */
+
+"use strict";
+
+const { Graph } = require("../../data-structures/graphs/graph");
+const {
+  PriorityQueue,
+} = require("../../data-structures/queues/priority-queue");
+const testingUtils = require("../../utilities/testing-utils");
+const testCases = [
+  [
+    [
+      "Fullstack",
+      "Starbucks",
+      "Insomnia Cookies",
+      "Cafe Grumpy",
+      "Dublinger",
+      "Dig Inn",
+    ],
+    [
+      ["Fullstack", "Dublinger", 2],
+      ["Fullstack", "Dig Inn", 7],
+      ["Fullstack", "Starbucks", 6],
+      ["Dublinger", "Starbucks", 3],
+      ["Dublinger", "Dig Inn", 4],
+      ["Dublinger", "Insomnia Cookies", 7],
+      ["Starbucks", "Insomnia Cookies", 6],
+      ["Cafe Grumpy", "Insomnia Cookies", 5],
+      ["Cafe Grumpy", "Dig Inn", 9],
+    ],
+    "Fullstack",
+    "Cafe Grumpy",
+    `Path = [${[
+      "Fullstack",
+      "Dublinger",
+      "Insomnia Cookies",
+      "Cafe Grumpy",
+    ].toString()}];
+    Time = 14 minutes`,
+  ],
+];
+function AStar() { }
+
+AStar.prototype.reconstructPath = function (cameFrom = {}, current = "", time = "") {
+  const totalPath = [current];
+  const keys = Object.keys(cameFrom).reverse();
+  for (const key of keys) {
+    current = cameFrom[key];
+    totalPath.unshift(current);
+  }
+  return `Path = [${totalPath}];
+  Time = ${time} minutes`;
+};
+
+AStar.prototype.heuristic = function (current) {
+  const guess = 2;
+  return current * guess;
+};
+AStar.prototype.search = function (graphInfo) {
+  // Create `graph` with `graphInfo`.
+  const nodes = graphInfo[0],
+    edges = graphInfo[1],
+    start = graphInfo[2],
+    end = graphInfo[3];
+
+  const graph = new Graph();
+
+  nodes.forEach((node) => graph.addNode(node));
+  edges.forEach((edge) => graph.addEdge(...edge));
+
+  const openSet = new PriorityQueue();
+  const cameFrom = {};
+
+  const gScore = {};
+  const fScore = {};
+
+  graph.nodes.forEach((node) => {
+    gScore[node] = fScore[node] = Infinity;
+  });
+
+  openSet.enqueue([start, 0]);
+  gScore[start] = 0;
+  fScore[start] = this.heuristic(gScore[start]);
+
+  while (!openSet.isEmpty()) {
+    let current;
+    let lowest = Infinity;
+
+    openSet.collection.forEach((node) => {
+      if (fScore[node[0]] < lowest) {
+        current = node;
+        lowest = fScore[node[0]];
+      }
+    });
+
+    if (current[0] === end) {
+      return this.reconstructPath(cameFrom, current[0], gScore[end]);
+    }
+
+    openSet.remove(current[0]);
+
+    graph.adjacencyList[current[0]].forEach((neighbor) => {
+      const tentativeGScore = gScore[current[0]] + neighbor.weight;
+      if (tentativeGScore < gScore[neighbor.node]) {
+        cameFrom[neighbor.node] = current[0];
+        gScore[neighbor.node] = tentativeGScore;
+        fScore[neighbor.node] = 
+          gScore[neighbor.node] + this.heuristic(neighbor.weight);
+        if (openSet.collection.indexOf([neighbor.node, neighbor.weight]) < 0) {
+          openSet.enqueue([neighbor.node, neighbor.weight]);
+        }
+      }
+    });
+  }
+  return "AStar did not reach the goal.";
+};
+
+const testAStar = (testCase) => {
+  const aStar = new AStar();
+  return aStar.search(testCase);
+}
+
+testingUtils.runTestsTo(testAStar, testCases);
+
+module.exports = exports = { AStar };
